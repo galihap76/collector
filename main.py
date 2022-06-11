666685513c949fb0e6868904915f7a69447750ca
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2022-06-11 18:16:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import argparse, requests, phonenumbers, time, os
+import argparse, requests, phonenumbers, time, subprocess
 from instaloader import *
 from phonenumbers import carrier, geocoder, timezone
 
@@ -28,85 +28,78 @@ class COLLECTOR:
     #this is main function
     def Main(self):
         if args.number:
-            try:
-                phone_number = phonenumbers.parse(args.number)
-                phone_number_national = phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.NATIONAL)
-                phone_number_international = phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
-                ISP = carrier.name_for_number(phone_number, 'en')
-                Time_zone = timezone.time_zones_for_number(phone_number) 
-                Country = geocoder.country_name_for_number(phone_number, 'en')
-                Location = geocoder.description_for_number(phone_number, 'en') 
-                print(f"[!] Fetching Phone Number : {args.number}")
-                print(f"[+] {phone_number}")
-                print(f"[+] International Format : {phone_number_international}")        
-                print(f"[+] National Format : {phone_number_national}")           
-                print(f"[+] Time Zone : {Time_zone}")
-                print(f"[+] ISP : {ISP}")
-                print(f"[+] Country Found : {Country}")
-            except phonenumbers.phonenumberutil.NumberParseException:
-                print("[-] Can't detect")
+            phone_number = phonenumbers.parse(args.number)
+            phone_number_national = phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.NATIONAL)
+            phone_number_international = phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+            ISP = carrier.name_for_number(phone_number, 'en')
+            Time_zone = timezone.time_zones_for_number(phone_number) 
+            Country = geocoder.country_name_for_number(phone_number, 'en')
+            Location = geocoder.description_for_number(phone_number, 'en') 
+            print(f"[!] Fetching Phone Number : {args.number}")
+            print(f"[+] {phone_number}")
+            print(f"[+] International Format : {phone_number_international}")        
+            print(f"[+] National Format : {phone_number_national}")           
+            print(f"[+] Time Zone : {Time_zone}")
+            print(f"[+] ISP : {ISP}")
+            print(f"[+] Country Found : {Country}")
                 
         elif args.github:
-            try:
-                username = args.github
-                url = f'https://api.github.com/users/{username}'
-                response = requests.get(url)
-                if response.status_code == 200 and requests.codes.ok:
-                    data = response.json()
-                    print( f'[!] Fetching On Github Account : {username}')
-                    for i in data:
-                        print(f'[+] {i} : ',data[i])
-            except requests.exceptions.ConnectionError:
-                print("[-] Error connecting")
-                       
-        elif args.ip:
-            try:    
-                ip = args.ip
-                url = f'https://ipapi.co/{ip}/json/'
-                response = requests.get(url)
+            username = args.github
+            url = f'https://api.github.com/users/{username}'
+            response = requests.get(url)
+            if response.status_code == 200 and requests.codes.ok:
                 data = response.json()
+                print( f'[!] Fetching On Github Account : {username}')
                 for i in data:
                     print(f'[+] {i} : ',data[i])
-            except requests.exceptions.ConnectionError:
-                print("[-] Error connecting")
+                       
+        elif args.ip:
+            ip = args.ip
+            url = f'https://ipapi.co/{ip}/json/'
+            response = requests.get(url)
+            data = response.json()
+            for i in data:
+                print(f'[+] {i} : ',data[i])
                 
         elif args.instagram:
-            try:
-                print('[!] Waiting...')
-                bot = instaloader.Instaloader()
-                username = args.instagram
-                profile = instaloader.Profile.from_username(bot.context, username)
-                posts = profile.get_posts()
-                private = profile.is_private
-                business = profile.is_business_account
-                url = profile.external_url
-                business_type = profile.business_category_name
-                profile = instaloader.Profile.from_username(bot.context, username)
-                print("[+] Username : ", profile.username)
-                print("[+] User ID : ", profile.userid)
-                print("[+] Number of Posts : ", profile.mediacount)
-                print("[+] Followers : ", profile.followers)
-                print("[+] Following : ", profile.followees)
-                print("[+] Bio : ", profile.biography,profile.external_url)
-                print(f'[+] Is business account : {business}')
-                print(f'[+] Business type : {business_type}')
-                print(f'[+] External url : {url}')
-                print(f'[+] Is private : {private}')
-                bot.download_profile(username, profile_pic_only = True)
-                for index, post in enumerate(posts, 1):
-                    bot.download_post(post, target=f"{profile.username}_{index}")
-            except:      
-                pass
+            print('[!] Waiting...')
+            bot = instaloader.Instaloader()
+            username = args.instagram
+            profile = instaloader.Profile.from_username(bot.context, username)
+            posts = profile.get_posts()
+            private = profile.is_private
+            business = profile.is_business_account
+            url = profile.external_url
+            business_type = profile.business_category_name
+            profile = instaloader.Profile.from_username(bot.context, username)
+            print("[+] Username : ", profile.username)
+            print("[+] User ID : ", profile.userid)
+            print("[+] Number of Posts : ", profile.mediacount)
+            print("[+] Followers : ", profile.followers)
+            print("[+] Following : ", profile.followees)
+            print("[+] Bio : ", profile.biography,profile.external_url)
+            print(f'[+] Is business account : {business}')
+            print(f'[+] Business type : {business_type}')
+            print(f'[+] External url : {url}')
+            print(f'[+] Is private : {private}')
+            bot.download_profile(username, profile_pic_only = True)
+            for index, post in enumerate(posts, 1):
+                bot.download_post(post, target=f"{profile.username}_{index}")
                 
 #RUN THE COLLECTOR
 if __name__ == "__main__":
     try:
         RUN = COLLECTOR()
         RUN.Main()
+    # Handling error
     except KeyboardInterrupt:
         print('^C')
+    except requests.exceptions.ConnectionError:
+        print("[-] Error connecting")
+    except phonenumbers.phonenumberutil.NumberParseException:
+        print("[-] Can't detect")
     except ModuleNotFoundError:
         print('[!] Collector need installed some modules')
         time.sleep(2)
         print('[!] Installing modules...')
-        os.system('pip install -r requirements.txt')
+        subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
